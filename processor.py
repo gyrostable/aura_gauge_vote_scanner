3bafcc57702d8d384c88f1eae234f23ac3cfce35
Xeonus <vanderbourg@gmail.com>
2024-06-20 15:10:22 +0200

    fix: correctly parse vote weight
    - add more columns for debugging
    - fix vote weight bug

--- processor.py
+++ processor.py
@@ -1,6 +1,10 @@
 # processor.py
 
 import pandas as pd
+from decimal import Decimal, getcontext
+
+# Set precision high enough to avoid rounding issues
+getcontext().prec = 28
 
 
 def process_votes(votes, choices, target_choices):
@@ -9,14 +13,21 @@ def process_votes(votes, choices, target_choices):
 
     filtered_votes = []
     for vote in votes:
-        matching_choices = [choice for choice in vote['choice'].keys() if int(choice) in target_indices]
+        choice_weights = vote['choice']
+        total_votes = sum(choice_weights.values())
+        matching_choices = [choice for choice in choice_weights.keys() if int(choice) in target_indices]
         if matching_choices:
-            num_choices = len(vote['choice'])
-            vp_per_choice = vote['vp'] / num_choices
+            total_vp = Decimal(vote['vp'])
+            total_vp_for_matching_choices = sum(
+                (Decimal(choice_weights[choice]) / Decimal(total_votes)) * total_vp
+                for choice in matching_choices
+            )
             filtered_votes.append({
                 'address': vote['voter'],
                 'choices': [choices[int(choice) - 1] for choice in matching_choices],
-                'voting_power': vp_per_choice * len(matching_choices)
+                'total_voting_power': float(total_vp),
+                'vote_weight': float(sum(choice_weights[choice] for choice in matching_choices) / total_votes),
+                'voting_power': float(total_vp_for_matching_choices)
             })
 
     return pd.DataFrame(filtered_votes)
