c11cd91b0ea98a7de35978493ec65b6d0ead8cef
Xeonus <vanderbourg@gmail.com>
2024-06-21 09:02:19 +0200

    refactor: output tables

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 # config.py
 
 # Define the proposal ID and target choices here
-PROPOSAL_ID = "0xf5f08d407b3643c6bdd0155d70729be7be8128153d74f3941872874564d3c056"
-TARGET_CHOICES = ["z-Gyroe USDC.e/GYD", "z-Gyroe USDT/GYD"]
+PROPOSAL_ID = "0x917f4e9d801a047c8f63b9ff60bf68141acd7865b4ef62b4d0fa58ab7738d7dd"
+TARGET_CHOICES = ["z-Gyroe USDC.e/GYD", "z-Gyroe USDT/GYD", "Gyroe USDC/GYD"]
+
+# Previous campaigns
+# Campaign 1: 8 SPIN per vote
+# PROPOSAL_ID = "0xf5f08d407b3643c6bdd0155d70729be7be8128153d74f3941872874564d3c056"
+# TARGET_CHOICES = ["z-Gyroe USDC.e/GYD", "z-Gyroe USDT/GYD"]
--- processor.py
+++ processor.py
@@ -9,25 +9,24 @@ getcontext().prec = 28
 
 def process_votes(votes, choices, target_choices):
     choice_indices = {choice: index + 1 for index, choice in enumerate(choices)}
-    target_indices = [choice_indices[choice] for choice in target_choices if choice in choice_indices]
+    target_indices = {index: choice for choice, index in choice_indices.items() if choice in target_choices}
 
     filtered_votes = []
     for vote in votes:
         choice_weights = vote['choice']
         total_votes = sum(choice_weights.values())
-        matching_choices = [choice for choice in choice_weights.keys() if int(choice) in target_indices]
-        if matching_choices:
-            total_vp = Decimal(vote['vp'])
-            total_vp_for_matching_choices = sum(
-                (Decimal(choice_weights[choice]) / Decimal(total_votes)) * total_vp
-                for choice in matching_choices
-            )
-            filtered_votes.append({
-                'address': vote['voter'],
-                'choices': [choices[int(choice) - 1] for choice in matching_choices],
-                'total_voting_power': float(total_vp),
-                'vote_weight': float(sum(choice_weights[choice] for choice in matching_choices) / total_votes),
-                'voting_power': float(total_vp_for_matching_choices)
-            })
+        total_vp = Decimal(vote['vp'])
+
+        for choice_index, weight in choice_weights.items():
+            if int(choice_index) in target_indices:
+                relative_weight = Decimal(weight) / Decimal(total_votes)
+                voting_power = relative_weight * total_vp
+                filtered_votes.append({
+                    'address': vote['voter'],
+                    'choice': target_indices[int(choice_index)],
+                    'total_voting_power': float(total_vp),
+                    'vote_weight': float(relative_weight),
+                    'total_votes': float(voting_power)
+                })
 
     return pd.DataFrame(filtered_votes)
